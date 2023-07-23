b151d8e079f55931574aac7a9e07f30bb78c7b42
Fluebubble <Trots.anatoly@gmail.com>
2023-07-23 19:08:55 +0300

    showMoreBtn script finished

--- src/scripts/gallery.js
+++ src/scripts/gallery.js
@@ -1,6 +1,6 @@
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-const lightbox = new SimpleLightbox('.certificates-gallery a', {
+export const lightbox = new SimpleLightbox('.certificates-gallery a', {
   /* options */
 });
 // const lightbox = new SimpleLightbox('.certificates-list .certificates-list__item a', {
--- src/scripts/showMoreDocs.js
+++ src/scripts/showMoreDocs.js
@@ -1,20 +1,22 @@
+import { lightbox } from './gallery';
+
 const certificates = [
-//   {
-//     name: 'Healing Trauma 2022',
-//     src: './img/cert/healing-trauma.jpg',
-//   },
-//   {
-//     name: 'Интеграция майндфулнес 2021',
-//     src: './img/cert/integration-mindfulness.webp',
-//   },
-//   {
-//     name: 'Когнитивно-поведенческая терапия 2021',
-//     src: './img/cert/cognitive-pov-therapy.jpg',
-//   },
-//   {
-//     name: 'Курс MBSR 2020',
-//     src: './img/cert/MBSR.png',
-//   },
+  //   {
+  //     name: 'Healing Trauma 2022',
+  //     src: './img/cert/healing-trauma.jpg',
+  //   },
+  //   {
+  //     name: 'Интеграция майндфулнес 2021',
+  //     src: './img/cert/integration-mindfulness.webp',
+  //   },
+  //   {
+  //     name: 'Когнитивно-поведенческая терапия 2021',
+  //     src: './img/cert/cognitive-pov-therapy.jpg',
+  //   },
+  //   {
+  //     name: 'Курс MBSR 2020',
+  //     src: './img/cert/MBSR.png',
+  //   },
   {
     name: 'Пропедевтика психиатрии 2020',
     src: './img/cert/proped-therapy.webp',
@@ -32,32 +34,45 @@ const certificates = [
     src: './img/cert/cognipov.webp',
   },
 ];
-
 const showMoreBtn = document.querySelector(
   '.certificates-gallery__loadMore-button'
 );
+let isMoreDocsShowed = false;
 
 const gallery = document.querySelector('.certificates-gallery');
-showMoreBtn.addEventListener('click', () => {
-  console.log(gallery);
-  const markup = [];
-  //   gallery.insertAdjacentElement('beforeend', '<p>kek</p>');
-  for (cert of certificates) {
-    let item = document.createElement('a');
-    item.classList.add('certificates-list__item-link');
-    item.href = cert.src;
+const addedDocs = [];
 
-    item.innerHTML = `<img
-          src="${cert.src}"
-          alt="${cert.name}"
-          class="certificates-list__image"
-        />
-        <p class="certificate-name">
-          ${cert.name}
-        </p>`;
-    markup.push(item);
-    console.log(item);
-    // console.log(markup);
+showMoreBtn.addEventListener('click', () => {
+  if (!isMoreDocsShowed) {
+    // console.log(gallery);
+    const markup = [];
+    for (cert of certificates) {
+      let item = document.createElement('a');
+      addedDocs.push(item);
+      item.classList.add('certificates-list__item-link');
+      item.href = cert.src;
+
+      item.innerHTML = `<img
+            src="${cert.src}"
+            alt="${cert.name}"
+            class="certificates-list__image"
+          />
+          <p class="certificate-name">
+            ${cert.name}
+          </p>`;
+      markup.push(item);
+      // console.log(item);
+    }
+    gallery.append(...markup);
+    isMoreDocsShowed = true;
+    showMoreBtn.textContent = 'Показать меньше';
+    lightbox.refresh();
+    // console.log(addedDocs);
+  } else {
+    addedDocs.forEach(doc => {
+      doc.remove();
+    });
+    showMoreBtn.textContent = 'Показать все документы';
+    isMoreDocsShowed = false;
   }
-  gallery.append(...markup);
 });
