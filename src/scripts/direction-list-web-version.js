07786d1ed858d7f6a0740edba38002e07de82eff
Fluebubble <Trots.anatoly@gmail.com>
2023-08-17 22:50:15 +0300

    directions js pc ver script finished

--- src/scripts/direction-list-web-version.js
+++ src/scripts/direction-list-web-version.js
@@ -1,19 +1,40 @@
-// import { log } from "handlebars";
+// Функция для обработки изменения ширины окна
+// function handleWindowResize() {
+//   const screenWidth = window.innerWidth;
 
-const directionsList = document.querySelector('.psy-directions-list');
-const directionsListElements = document.querySelectorAll(
-  '.psy-direction__item'
-);
-const newDirectionsListElements = [];
-console.log(directionsListElements);
+//   if (screenWidth < 768) {
+//       // Выполнить код для экранов шириной меньше 768px
+//       console.log("Экран меньше 768px" + window.innerWidth);
+//   } else if (screenWidth >= 768 && screenWidth < 1200) {
+//       // Выполнить код для экранов шириной от 768px до 1199px
+//       console.log("Экран от 768px до 1199px" + window.innerWidth);
+//   } else {
+//       // Выполнить код для экранов шириной 1200px и больше
+//       console.log("Экран 1200px и больше" + window.innerWidth);
+//   }
+// }
 
-const secondDirectionsList = document.createElement('dl');
+// Вызвать функцию при загрузке страницы и при изменении размера окна
+// window.addEventListener('load', handleWindowResize);
+// window.addEventListener('resize', handleWindowResize);
 
-for (let i = 1; i < directionsListElements.length; i += 2) {
-  newDirectionsListElements.push(directionsListElements[i]);
+if (window.innerWidth >= 1200) {
+  const directionsList = document.querySelector('.psy-directions-list');
+  const directionsListElements = document.querySelectorAll(
+    '.psy-direction__item'
+  );
+  const newDirectionsListElements = [];
+  console.log(directionsListElements);
+
+  const secondDirectionsList = document.createElement('dl');
+  secondDirectionsList.classList.add('psy-directions-list');
+
+  for (let i = 1; i < directionsListElements.length; i += 2) {
+    newDirectionsListElements.push(directionsListElements[i]);
+  }
+  console.log(newDirectionsListElements);
+
+  secondDirectionsList.append(...newDirectionsListElements);
+  directionsList.after(secondDirectionsList);
+  console.log(secondDirectionsList);
 }
-console.log(newDirectionsListElements);
-
-secondDirectionsList.append(...newDirectionsListElements);
-directionsList.after(secondDirectionsList);
-console.log(secondDirectionsList);
