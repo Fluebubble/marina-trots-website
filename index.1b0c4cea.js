ab20453712deadc19ff016b12e6a97ce7bcae905
Fluebubble <Trots.anatoly@gmail.com>
2023-10-07 20:34:37 +0000

    Deploying to gh-pages from @ Fluebubble/marina-trots-website@312afb0c27b8d6a37f25b4fbedc09afdf18483ba 🚀

---
+++ index.1b0c4cea.js
@@ -0,0 +1,2 @@
+!function(){var e=document.forms.form;e.addEventListener("submit",(function(o){o.preventDefault(),fetch("https://script.google.com/macros/s/AKfycbxxBiH36WxGDlq6xYj10qYS-ddVKRxlP84ztelPhc4ryf1oe07nf_IoJ8-11_dzDEyewA/exec",{method:"POST",body:new FormData(e)}).then((function(e){return console.log("Success!",e)})).catch((function(e){return console.error("Error!",e.message)}))}))}();
+//# sourceMappingURL=index.1b0c4cea.js.map
