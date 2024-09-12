b64ffc2897fbe409c3fca2c13201c624a8250cde
Fluebubble <Trots.anatoly@gmail.com>
2024-09-12 13:05:23 +0200

    top bar finished

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,61 @@
+import { Container } from '../Container/Container';
+import styles from './Header.module.scss';
+import { MessangerList } from './MessangerList/MessangerList';
+import { PhoneCTA } from './PhoneCTA/PhoneCTA';
+
+export const Header = () => {
+  return (
+    <header className="header">
+      {/* <div className="container"> */}
+      <Container>
+        <div className={styles['header__inner']}>
+          <MessangerList />
+          {/* <ul className={styles['header__socials-list']}>
+            <li className="header__socials-item">
+              <a
+                href="https://t.me/Marinatrots"
+                className="header__socials-link"
+                target="_blank"
+              >
+                <svg className="header__socials-image">
+                  <use href="./img/sprite/symbol-defs-opti.svg#telegram"></use>
+                </svg>
+              </a>
+            </li>
+            <li className="header__socials-item">
+              <a
+                href="https://api.whatsapp.com/send?phone=380631095844"
+                className="header__socials-link"
+                target="_blank"
+              >
+                <svg className="header__socials-image">
+                  <use href="./img/sprite/symbol-defs-opti.svg#whatapp"></use>
+                </svg>
+              </a>
+            </li>
+            <li className="header__socials-item">
+              <a
+                href="viber://chat?number=%2B380631095844"
+                className="header__socials-link"
+                target="_blank"
+              >
+                <svg className="header__socials-image">
+                  <use href="./img/sprite/symbol-defs-opti.svg#viber"></use>
+                </svg>
+              </a>
+            </li>
+          </ul> */}
+          <PhoneCTA />
+          {/* <a
+            href="tel:+380631095844"
+            className="phone"
+          >
+            <span className="phone__cta">Остались вопросы? Звоните!</span>
+            <span className="phone__number">+38 063 109 58 44</span>
+          </a> */}
+        </div>
+      </Container>
+      {/* </div> */}
+    </header>
+  );
+};
---
+++ src/components/Header/MessangerList/MessangerList.tsx
@@ -0,0 +1,59 @@
+import { MessangerItem } from './MessangerItem/MessangerItem';
+import styles from './MessangerList.module.scss';
+import { MessangerIconsNames } from '../../../types/messangerIconsNames';
+
+// interface Props {
+//   children: React.ReactNode;
+// }
+
+export const MessangerList = () => {
+  return (
+    <ul className={styles['messanger-list']}>
+      <MessangerItem
+        link="https://t.me/Marinatrots"
+        icon={MessangerIconsNames.Telegram}
+      />
+      <MessangerItem
+        link="https://api.whatsapp.com/send?phone=380631095844"
+        icon={MessangerIconsNames.WhatsApp}
+      />
+      <MessangerItem
+        link="viber://chat?number=%2B380631095844"
+        icon={MessangerIconsNames.Viber}
+      />
+      {/* <li className="header__socials-item">
+        <a
+          href="https://t.me/Marinatrots"
+          className="header__socials-link"
+          target="_blank"
+        >
+          <svg className="header__socials-image">
+            <use href="./img/sprite/symbol-defs-opti.svg#telegram"></use>
+          </svg>
+        </a>
+      </li>
+      <li className="header__socials-item">
+        <a
+          href="https://api.whatsapp.com/send?phone=380631095844"
+          className="header__socials-link"
+          target="_blank"
+        >
+          <svg className="header__socials-image">
+            <use href="./img/sprite/symbol-defs-opti.svg#whatapp"></use>
+          </svg>
+        </a>
+      </li>
+      <li className="header__socials-item">
+        <a
+          href="viber://chat?number=%2B380631095844"
+          className="header__socials-link"
+          target="_blank"
+        >
+          <svg className="header__socials-image">
+            <use href="./img/sprite/symbol-defs-opti.svg#viber"></use>
+          </svg>
+        </a>
+      </li> */}
+    </ul>
+  );
+};
