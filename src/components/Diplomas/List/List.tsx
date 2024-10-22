c79fa8b3292f45621da71fcbc853300ef600b187
Fluebubble <Trots.anatoly@gmail.com>
2024-10-22 16:41:40 +0200

    diploma grid changed + autoplay added

--- src/components/Diplomas/List/List.tsx
+++ src/components/Diplomas/List/List.tsx
@@ -3,7 +3,7 @@ import "swiper/css";
 import "swiper/css/grid";
 import "swiper/css/pagination";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Grid, Pagination } from "swiper/modules";
+import { Grid, Pagination, Autoplay } from "swiper/modules";
 import { Item } from "./Item/Item";
 import { Certificate } from "@/types/certificate";
 import { DeviceSizes } from "../../../types/deviceSizes";
@@ -23,19 +23,24 @@ export const List: React.FC<Props> = ({ certificates }) => {
   return (
     <div className={styles.content}>
       <Swiper
+        speed={500}
         spaceBetween={10}
         slidesPerView={2}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: true,
+        }}
         grid={{
           fill: "row",
           rows: 2,
         }}
         pagination={paginationSettings}
-        modules={[Grid, Pagination]}
+        modules={[Grid, Pagination, Autoplay]}
         breakpoints={{
           [DeviceSizes.desktop]: {
             slidesPerView: 3,
             grid: {
-              rows: 3,
+              rows: 2,
             },
           },
         }}
