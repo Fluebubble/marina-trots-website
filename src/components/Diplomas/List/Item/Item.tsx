c55c929661fd093390894b29eb7f516a35503e8a
Fluebubble <Trots.anatoly@gmail.com>
2024-10-22 17:54:52 +0200

    added lightbox

--- src/components/Diplomas/List/Item/Item.tsx
+++ src/components/Diplomas/List/Item/Item.tsx
@@ -2,13 +2,38 @@ import React from "react";
 import styles from "./Item.module.scss";
 import { Certificate } from "../../../../types/certificate";
 
-export const Item: React.FC<Omit<Certificate, "id">> = ({ image, thumb, alt, title }) => {
+interface Props extends Certificate {
+  setCurrentSlideId: React.Dispatch<
+    React.SetStateAction<Pick<Certificate, "id"> | null>
+  >;
+}
+
+export const Item: React.FC<Props> = ({
+  image,
+  thumb,
+  alt,
+  title,
+  id,
+  setCurrentSlideId,
+}) => {
+  const imageClickHandler = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    id: Pick<Certificate, "id">
+  ) => {
+    e.preventDefault();
+    setCurrentSlideId(id);
+  };
+
   return (
-      <a href={image} className={styles.link}>
-        <div className={styles["image-container"]}>
-          <img src={thumb} alt={alt} className={styles.image} />
-        </div>
-        <p className={styles.name}>{title}</p>
-      </a>
+    <a
+      href={image}
+      className={styles.link}
+      onClick={(e) => imageClickHandler(e, {id})}
+    >
+      <div className={styles["image-container"]}>
+        <img src={thumb} alt={alt} className={styles.image} />
+      </div>
+      <p className={styles.name}>{title}</p>
+    </a>
   );
 };
--- src/components/Diplomas/List/List.tsx
+++ src/components/Diplomas/List/List.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "swiper/css";
 import "swiper/css/grid";
 import "swiper/css/pagination";
@@ -8,6 +8,8 @@ import { Item } from "./Item/Item";
 import { Certificate } from "@/types/certificate";
 import { DeviceSizes } from "../../../types/deviceSizes";
 import styles from "./List.module.scss";
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 interface Props {
   certificates: Certificate[];
@@ -19,6 +21,20 @@ export const List: React.FC<Props> = ({ certificates }) => {
     clickable: true,
     el: paginationRef.current,
   };
+
+  const lightboxSlides = certificates.map(({ alt, image, id }) => ({
+    src: image,
+    alt,
+    id,
+  }));
+
+  // const [open, setOpen] = useState(false);
+  const [currentSlideId, setCurrentSlideId] = useState<Pick<
+    Certificate,
+    "id"
+  > | null>(null);
+
+  console.dir(lightboxSlides);
 
   return (
     <div className={styles.content}>
@@ -54,11 +70,21 @@ export const List: React.FC<Props> = ({ certificates }) => {
               image={cert.image}
               alt={cert.alt}
               thumb={cert.thumb}
+              id={cert.id}
+              setCurrentSlideId={setCurrentSlideId}
             />
           </SwiperSlide>
         ))}
         <div ref={paginationRef} className={styles.pagination}></div>
       </Swiper>
+      <Lightbox
+        open={currentSlideId !== null}
+        close={() => setCurrentSlideId(null)}
+        slides={lightboxSlides}
+      />
+      {/* <button type="button" onClick={() => setOpen(true)}>
+        Open Lightbox
+      </button> */}
     </div>
   );
 };
