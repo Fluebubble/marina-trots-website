bd4c4cc6a6edc1b503142af4c5fce129d4ede0f3
Fluebubble <Trots.anatoly@gmail.com>
2024-10-28 21:05:54 +0100

    Directions list component separated as FAQList

--- src/components/FAQList/FAQList.tsx
+++ src/components/FAQList/FAQList.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import styles from "./List.module.scss";
-import data from "../../../data/psy-directions.json";
-import { ListItem } from "./ListItem/ListItem";
+import styles from "./FAQList.module.scss";
+import data from "../../data/psy-directions.json";
+import { ListItem } from "./FAQItem/FAQItem";
 
-export const List = () => {
+export const FAQList = () => {
   return (
     <div className={styles.wrapper}>
       <dl className={styles.list}>
--- src/components/PsyDirections/PsyDirections.tsx
+++ src/components/PsyDirections/PsyDirections.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 // import styles from "./PsyDirections.module.scss";
 import { Title } from "../Title/Title";
-import { List } from "./List/List";
+import { FAQList } from "../FAQList/FAQList";
 
 export const PsyDirections = () => {
   return (
     <section className="section psy-directions">
       <Title text="Направления с которыми я работаю" />
-      <List />
+      <FAQList />
     </section>
   );
 };
