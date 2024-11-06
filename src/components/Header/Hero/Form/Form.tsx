36af583001ea668e15ce9c2dd7c6b0320e03da97
Anatoliy Trots <Trots.anatoly@gmail.com>
2024-11-06 14:36:30 +0100

    header form configured

--- src/components/Header/Hero/Form/Form.tsx
+++ src/components/Header/Hero/Form/Form.tsx
@@ -1,27 +1,34 @@
-import React from 'react'
-import styles from './Form.module.scss';
+import React, { useState } from "react";
+import styles from "./Form.module.scss";
+import { sendDataToBot } from "../../../../api/form-to-telegram";
 
 export const Form = () => {
+  const [phone, setPhone] = useState("");
 
+  const submitFormHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    sendDataToBot("Noname", phone);
+
+    setPhone("");
+  };
 
   return (
-      <form
-        className={styles.form}
-        name="contact-form"
-        id="hero-form"
-      >
-        <input
-          type="number"
-          className={styles.input}
-          placeholder="Ваш номер"
-          name="number"
-        />
-        <button
-          type="submit"
-          className={styles.button}
-        >
-          Оставить заявку
-        </button>
-      </form>
+    <form
+      className={styles.form}
+      name="contact-form"
+      id="hero-form"
+      onSubmit={submitFormHandler}
+    >
+      <input
+        type="number"
+        className={styles.input}
+        placeholder="Ваш номер"
+        name="number"
+        value={phone}
+        onChange={(e) => setPhone(e.target.value)}
+      />
+      <button className={styles.button}>Оставить заявку</button>
+    </form>
   );
 };
