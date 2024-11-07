d5e362093e8301cee74d55cf6088c59ab601ccd7
Anatoliy Trots <Trots.anatoly@gmail.com>
2024-11-07 20:57:33 +0100

    Updates

--- index.abb5a119.js
+++
@@ -1,2 +0,0 @@
-if(window.innerWidth>=1200){const e=document.querySelector(".psy-directions__list"),o=document.querySelectorAll(".psy-direction__item"),t=[];console.log(o);const l=document.createElement("dl");l.classList.add("psy-directions__list");for(let e=1;e<o.length;e+=2)t.push(o[e]);console.log(t),l.append(...t),e.after(l),console.log(l)}
-//# sourceMappingURL=index.abb5a119.js.map
--- index.bb7de5e7.js
+++
@@ -1,2 +0,0 @@
-const e=document.forms.form;e.addEventListener("submit",(o=>{o.preventDefault(),fetch("https://script.google.com/macros/s/AKfycbxxBiH36WxGDlq6xYj10qYS-ddVKRxlP84ztelPhc4ryf1oe07nf_IoJ8-11_dzDEyewA/exec",{method:"POST",body:new FormData(e)}).then((e=>console.log("Success!",e))).catch((e=>console.error("Error!",e.message)))}));
-//# sourceMappingURL=index.bb7de5e7.js.map
