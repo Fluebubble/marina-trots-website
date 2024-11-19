9f62baf9b370703ace36bc7277328ca8427564a3
Fluebubble <Trots.anatoly@gmail.com>
2024-11-19 14:29:38 +0100

    changed hero title

--- src/components/Header/Hero/MainTitle/MainTitle.tsx
+++ src/components/Header/Hero/MainTitle/MainTitle.tsx
@@ -4,8 +4,8 @@ import styles from './MainTitle.module.scss';
 export const MainTitle = () => {
   return (
     <h1 className={styles['hero__title-text']}>
-      <span className={styles['hero__title-first-part']}>Психолог</span>
-      <span className={styles['hero__title-second-part']}>Марина Троц</span>
+      <span className={styles['hero__title-first-part']}>Марина Троц</span>
+      <span className={styles['hero__title-second-part']}>Психолог, КПТ-терапевт</span>
     </h1>
   );
 };
