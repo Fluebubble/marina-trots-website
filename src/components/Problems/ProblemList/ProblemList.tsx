5f4a5068617c113c3c7fb872256c5a1fe1e2f8d9
Fluebubble <Trots.anatoly@gmail.com>
2024-11-22 19:58:41 +0100

    problems data moved to json

--- src/components/Problems/ProblemList/ProblemList.tsx
+++ src/components/Problems/ProblemList/ProblemList.tsx
@@ -2,17 +2,28 @@ import React from "react";
 import { ListItem } from "./ListItem/ListItem";
 import styles from "./ProblemList.module.scss";
 
-export const ProblemList = () => {
+interface Props {
+  problems: {
+    text: string;
+    image: string;
+    id: number;
+  }[];
+}
+
+export const ProblemList: React.FC<Props> = ({ problems }) => {
   return (
     <ul className={styles.list}>
-      <ListItem text="Депресія" image="#stress_2" />
+      {problems.map(({ text, image, id }) => (
+        <ListItem key={id} text={text} image={image} />
+      ))}
+      {/* <ListItem text="Депресія" image="#stress_2" />
       <ListItem text="Стреси різної складності" image="#stress_1" />
       <ListItem text="Сексуальні проблеми" image="#kiss" />
       <ListItem text="Тривожність, фобії та страхія" image="#anxiety" />
       <ListItem text="Панічні атаки" image="#panic" />
       <ListItem text="Низька самооцінка" image="#mirror" />
       <ListItem text="Психосоматичні захворювання" image="#pain" />
-      <ListItem text="Нав'язливі думки та дії" image="#repeat" />
+      <ListItem text="Нав'язливі думки та дії" image="#repeat" /> */}
     </ul>
   );
 };
--- src/components/Problems/Problems.tsx
+++ src/components/Problems/Problems.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { ProblemList } from './ProblemList/ProblemList';
-import { Title } from '../Title/Title';
+import React from "react";
+import { ProblemList } from "./ProblemList/ProblemList";
+import { Title } from "../Title/Title";
+import data from "./data.json";
 
 export const Problems = () => {
   return (
     <section className="section section__problems">
       <Title text="З якими проблемами я працюю" />
-      <ProblemList />
+      <ProblemList problems={data} />
     </section>
   );
 };
